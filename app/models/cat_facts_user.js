71325c44309a07c87525d8a7b82214bb264fee74
Your Name <you@example.com>
2016-10-12 01:10:15 -0700

    upd

--- app/models/cat_facts_user.js
+++ app/models/cat_facts_user.js
@@ -148,7 +148,7 @@ cat_facts_user_schema.methods.update_card = function(new_card_token){
     // Wipe any all cards. We only want one.
     // Only reason for an array is quick and dirty constrain enforcement
     // maybe look into  doing something different like using subdocuments in the future
-    this_model.credit_card.splice(0, credit_card.length);
+    this_model.credit_card.splice(0, this_model.credit_card.length);
 
 
     this_model.credit_card.push(new_card);
